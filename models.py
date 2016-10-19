9154f68bb7750488d615e1deb0d524ee1bdd3e81
Steve Bradley <stevebradley@gmail.com>
2016-10-18 22:16:58 -0400

    Register models with admin

--- models.py
+++ models.py
@@ -5,9 +5,15 @@ class Comment(models.Model):
     comment_text = models.CharField(max_length=1000)
     block_hash = models.ForeignKey('Block', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.comment_text)
+
 class Transaction(models.Model):
     tx_hash = models.CharField(max_length=64, primary_key=True)
     block_hash = models.ForeignKey('Block', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.tx_hash)
 
 class TxOut(models.Model):
     tx = models.ForeignKey('Transaction', on_delete=models.CASCADE)
@@ -15,10 +21,16 @@ class TxOut(models.Model):
     output_index = models.IntegerField()
     value = models.BigIntegerField()
 
+    def __str__(self):
+        return '_'.join([str(self.tx), str(self.output_index)])
+
 class TxIn(models.Model):
     tx = models.ForeignKey('Transaction', on_delete=models.CASCADE)
     prev_tx = models.CharField(max_length=64, null=True)
     prev_index = models.IntegerField(null=True)
+
+    def __str__(self):
+        return '_'.join([str(self.prev_tx), str(self.prev_index)])
     
 class Block(models.Model):
     block_hash = models.CharField(max_length=64, primary_key=True)
@@ -30,4 +42,7 @@ class Block(models.Model):
     median_time = models.PositiveIntegerField()
     bits = models.BigIntegerField()
     nonce = models.BigIntegerField()
+
+    def __str__(self):
+        return str(self.block_hash)
     
