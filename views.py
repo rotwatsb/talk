5a54a2c8f5a51ce9f0f5e14455eee3c2b57dd363
Steve Bradley <stevebradley@gmail.com>
2016-12-05 21:30:01 -0500

    Pre-restapi commit

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
 
 #from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
